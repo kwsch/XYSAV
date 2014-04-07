e841ecad43b715f7c963e2a5003c1665c18775dc
Kurt <kaphotics@gmail.com>
2014-04-07 13:12:46 -0700

    Initial
    This project is abandoned (obsoleted), serves as a reference for others
    wanting to do something similar with 3DS save files.

---
+++ XYSAV/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace XYSAV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.T_Offset = new System.Windows.Forms.TextBox();
+            this.T_Length = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.RTB = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(378, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.T_Offset);
+            this.groupBox1.Controls.Add(this.T_Length);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.RTB);
+            this.groupBox1.Enabled = false;
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(354, 223);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tools";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Length";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Offset";
+            // 
+            // T_Offset
+            // 
+            this.T_Offset.Location = new System.Drawing.Point(55, 19);
+            this.T_Offset.Name = "T_Offset";
+            this.T_Offset.Size = new System.Drawing.Size(100, 20);
+            this.T_Offset.TabIndex = 3;
+            this.T_Offset.Text = "05400";
+            // 
+            // T_Length
+            // 
+            this.T_Length.Location = new System.Drawing.Point(55, 45);
+            this.T_Length.Name = "T_Length";
+            this.T_Length.Size = new System.Drawing.Size(100, 20);
+            this.T_Length.TabIndex = 2;
+            this.T_Length.Text = "000002C8";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "CHK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RTB
+            // 
+            this.RTB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RTB.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RTB.Location = new System.Drawing.Point(0, 80);
+            this.RTB.Name = "RTB";
+            this.RTB.Size = new System.Drawing.Size(353, 142);
+            this.RTB.TabIndex = 0;
+            this.RTB.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(242, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 46);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Check All";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 262);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "XYSAV";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox RTB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox T_Offset;
+        private System.Windows.Forms.TextBox T_Length;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ XYSAV/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace XYSAV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public byte[] sav = new Byte[0x100000];
+
+        private UInt16 ccitt16(byte[] data)
+        {
+            ushort crc = 0xFFFF;
+            for (int i = 0; i < data.Length; i++)
+            {
+                crc ^= (ushort)(data[i] << 8);
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((crc & 0x8000) > 0)
+                    {
+                        crc = (ushort)((crc << 1) ^ 0x1021);
+                    }
+                    else
+                    {
+                        crc <<= 1;
+                    }
+                }
+            }
+            return crc;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Open Save File
+            OpenFileDialog opensav = new OpenFileDialog();
+            opensav.Filter = "SAV|*.sav;*.bin";
+            DialogResult result = opensav.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string path = opensav.FileName;
+                string ext = Path.GetExtension(path);
+                sav = File.ReadAllBytes(path);
+                if (((sav.Length == 0x100000)))
+                {
+                    // Unlock GB
+                    groupBox1.Enabled = true;
+                }
+                else
+                {
+                    string message = "Did not select a valid SAV";
+                    string caption = "Input Error";
+                    MessageBox.Show(message, caption);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create Checksum
+            uint offset = ToUInt32(T_Offset.Text);
+            uint length = ToUInt32(T_Length.Text);
+            byte[] data = new Byte[length];
+            Array.Copy(sav, offset, data, 0, length);
+            for (int i = 0; i < length; i++)
+            {
+                data[i] = sav[i + offset];
+            }
+            RTB.Text = ccitt16(data).ToString("X4");
+        }
+        private static uint ToUInt32(String value)
+        {
+            if (String.IsNullOrEmpty(value))
+                return 0;
+            return Convert.ToUInt32(value, 16);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Check all
+            RTB.Text = "";
+            int invalid = 0;
+            uint[] start =  {
+                               0x05400,	0x05800,	0x06400,	0x06600,	0x06800,	0x06A00,	0x06C00,	0x06E00,	0x07000,	0x07200,	0x07400,	0x09600,	0x09800,	0x09E00,	0x0A400,	0x0F400,	0x14400,	0x19400,	0x19600,	0x19E00,	0x1A400,	0x1AC00,	0x1B400,	0x1B600,	0x1B800,	0x1BE00,	0x1C000,	0x1C400,	0x1CC00,	0x1CE00,	0x1D000,	0x1D200,	0x1D400,	0x1D600,	0x1DE00,	0x1E400,	0x1E800,	0x20400,	0x20600,	0x20800,	0x20C00,	0x21000,	0x22C00,	0x23000,	0x23800,	0x23C00,	0x24600,	0x24A00,	0x25200,	0x26000,	0x26200,	0x26400,	0x27200,	0x27A00,	0x5C600,
+                            };
+            uint[] length = {
+                                0x000002C8,	0x00000B88,	0x0000002C,	0x00000038,	0x00000150,	0x00000004,	0x00000008,	0x000001C0,	0x000000BE,	0x00000024,	0x00002100,	0x00000140,	0x00000440,	0x00000574,	0x00004E28,	0x00004E28,	0x00004E28,	0x00000170,	0x0000061C,	0x00000504,	0x000006A0,	0x00000644,	0x00000104,	0x00000004,	0x00000420,	0x00000064,	0x000003F0,	0x0000070C,	0x00000180,	0x00000004,	0x0000000C,	0x00000048,	0x00000054,	0x00000644,	0x000005C8,	0x000002F8,	0x00001B40,	0x000001F4,	0x000001F0,	0x00000216,	0x00000390,	0x00001A90,	0x00000308,	0x00000618,	0x0000025C,	0x00000834,	0x00000318,	0x000007D0,	0x00000C48,	0x00000078,	0x00000200,	0x00000C84,	0x00000628,	0x00034AD0,	0x0000E058,
+                            };
+
+            int csoff = 0x6A81A;
+
+            if (ModifierKeys == Keys.Control)
+            {
+                csoff += 0x7F000;
+                for (int i = 0; i < start.Length; i++)
+                {
+                    start[i] += 0x7F000;
+                }
+            }
+
+            for (int i = 0; i < length.Length; i++)
+            {
+
+                byte[] data = new Byte[length[i]];
+                Array.Copy(sav, start[i], data, 0, length[i]);
+                ushort checksum = ccitt16(data);
+                ushort actualsum = (ushort)(sav[csoff + i * 0x8] + sav[csoff + i * 0x8 + 1] * 0x100);
+                RTB.Text += i.ToString("X2") + " - " + start[i].ToString("X5") + " - " + length[i].ToString("X8") + " - " + checksum.ToString("X4") + " - " + actualsum.ToString("X4");
+                if (checksum != actualsum)
+                {
+                    RTB.Text += " - INVALID.\r\n";
+                    invalid++;
+                }
+                else { RTB.Text += "\r\n"; }
+            }
+            RTB.Text += "Done. " + (0x37-invalid).ToString() + "/" + 0x37.ToString();
+        }
+    }
+}
